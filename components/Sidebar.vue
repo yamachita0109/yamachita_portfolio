a85fd8415a3dc53e47022d63e3aaa4fa85b10e01
Yamashita Kazumasa <yamachita0109@gmail.com>
2022-06-10 20:57:12 +0900

    1st commit.

---
+++ components/Sidebar.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="ag-wheel-block">
+    <ul id="pg-wheel" class="ag-wheel_list">
+      <li id="pg-wheel_page-name-1" class="pg-wheel_page-name ag-wheel_page-name pg-ag-active">
+        <a href="#pg-wheel_page-anchor-1">
+          <span class="ag-wheel-page_num">01</span>
+          <div class="ag-wheel-page_name">Section 1</div>
+        </a>
+      </li>
+      <li id="pg-wheel_page-name-2" class="pg-wheel_page-name ag-wheel_page-name">
+        <a href="#pg-wheel_page-anchor-2">
+          <span class="ag-wheel-page_num">02</span>
+          <div class="ag-wheel-page_name">Section 2</div>
+        </a>
+      </li>
+      <li id="pg-wheel_page-name-3" class="pg-wheel_page-name ag-wheel_page-name">
+        <a href="#pg-wheel_page-anchor-3">
+          <span class="ag-wheel-page_num">03</span>
+          <div class="ag-wheel-page_name">Section 3</div>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    index: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+  },
+  watch: {
+    index(val: number) {
+      document.querySelectorAll(".pg-wheel_page-name").forEach((e) => e.classList.remove("pg-ag-active"));
+      document.querySelector(`#pg-wheel_page-name-${val + 1}`)?.classList.add("pg-ag-active");
+      (document.querySelector("#pg-wheel") as HTMLElement)!.style.transform = `rotate(${(0 === val ? 0 : -22.5 * (val))}deg)`;
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.ag-wheel-block {
+  margin-top: -400px;
+  z-index: 1;
+  position: fixed;
+  left: 0;
+  top: 50%;
+  font-family: Acrom, sans-serif;
+}
+
+.ag-wheel_list {
+  height: 800px;
+  width: 800px;
+  margin: 0 50px 50px -680px;
+  border: 1px solid rgb(118, 118, 118);
+  font-size: 36px;
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  border-radius: 50%;
+  -webkit-transform-origin: 50% 50%;
+  -moz-transform-origin: 50% 50%;
+  -ms-transform-origin: 50% 50%;
+  -o-transform-origin: 50% 50%;
+  transform-origin: 50% 50%;
+  -webkit-transition: all 1s ease;
+  -moz-transition: all 1s ease;
+  -o-transition: all 1s ease;
+  transition: all 1s ease;
+  position: relative;
+  list-style: none;
+}
+
+.ag-wheel_page-name {
+  cursor: pointer;
+  transition: all 1s ease;
+  position: absolute;
+
+  a {
+    text-decoration: none;
+    color: rgb(118, 118, 118);
+  }
+
+  &:hover a {
+    color: #FFF;
+  }
+}
+
+.pg-ag-active a {
+  cursor: default;
+  color: #FFF;
+}
+
+.ag-wheel_page-name {
+  &:first-child {
+    top: 377px;
+    left: 797px;
+  }
+
+  &:nth-child(2) {
+    top: 561px;
+    left: 761px;
+    -webkit-transform: rotate(22.5deg);
+    -moz-transform: rotate(22.5deg);
+    -ms-transform: rotate(22.5deg);
+    -o-transform: rotate(22.5deg);
+    transform: rotate(22.5deg);
+  }
+
+  &:nth-child(3) {
+    top: 718px;
+    left: 657px;
+    -webkit-transform: rotate(45deg);
+    -moz-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    -o-transform: rotate(45deg);
+    transform: rotate(45deg);
+  }
+
+  &:nth-child(4) {
+    top: 823px;
+    left: 501px;
+    -webkit-transform: rotate(67.5deg);
+    -moz-transform: rotate(67.5deg);
+    -ms-transform: rotate(67.5deg);
+    -o-transform: rotate(67.5deg);
+    transform: rotate(67.5deg);
+  }
+
+  &:nth-child(5) {
+    top: 860px;
+    left: 316px;
+    -webkit-transform: rotate(90deg);
+    -moz-transform: rotate(90deg);
+    -ms-transform: rotate(90deg);
+    -o-transform: rotate(90deg);
+    transform: rotate(90deg);
+  }
+
+  &:nth-child(6) {
+    top: 824px;
+    left: 133px;
+    -webkit-transform: rotate(112.5deg);
+    -moz-transform: rotate(112.5deg);
+    -ms-transform: rotate(112.5deg);
+    -o-transform: rotate(112.5deg);
+    transform: rotate(112.5deg);
+  }
+
+  &:nth-child(7) {
+    top: 719px;
+    left: -21px;
+    -webkit-transform: rotate(135deg);
+    -moz-transform: rotate(135deg);
+    -ms-transform: rotate(135deg);
+    -o-transform: rotate(135deg);
+    transform: rotate(135deg);
+  }
+
+  &:nth-child(8) {
+    top: 564px;
+    left: -128px;
+    -webkit-transform: rotate(157deg);
+    -moz-transform: rotate(157deg);
+    -ms-transform: rotate(157deg);
+    -o-transform: rotate(157deg);
+    transform: rotate(157deg);
+  }
+}
+
+.ag-wheel-page_num {
+  float: left;
+  padding: 0 15px;
+  position: relative;
+
+  &:before {
+    content: "";
+    height: 7px;
+    width: 7px;
+    background-color: rgb(118, 118, 118);
+    position: absolute;
+    top: 18px;
+    left: 0;
+    -webkit-border-radius: 50%;
+    -moz-border-radius: 50%;
+    border-radius: 50%;
+  }
+}
+
+.pg-ag-active .ag-wheel-page_num:before {
+  height: 13px;
+  width: 13px;
+  margin: -3px 0 0 -3px;
+  background-color: #FFF;
+}
+
+.ag-wheel-page_name {
+  white-space: nowrap;
+  font-size: 36px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <section class="container">
+      <full-page :options="options">
+        <section class="section" id="section-1" data-anchor="pg-wheel_page-anchor-1">
+          <div class="ag-section_title">Section 1</div>
+        </section>
+        <section class="section" id="section-2" data-anchor="pg-wheel_page-anchor-2">
+          <div class="ag-section_title">Section 2</div>
+        </section>
+        <section class="section" id="section-3" data-anchor="pg-wheel_page-anchor-3">
+          <div class="ag-section_title">Section 3</div>
+        </section>
+      </full-page>
+    </section>
+
+    <Sidebar :index="index"></Sidebar>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+    // @ts-ignore
+    data() {
+      return {
+        index: 0 as Number,
+        options: {
+          verticalCentered: false,
+          css3: true,
+          responsiveWidth: 0,
+          responsiveHeight: 0,
+          responsiveSlides: false,
+          // @ts-ignore
+          onLeave: this.onLeave,
+        },
+      };
+    },
+    methods: {
+      onLeave(origin: any, destination: any, direction: any) {
+        // @ts-ignore
+        this.index = Number(destination.index)
+      },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+}
+.section {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 25px;
+}
+
+.ag-section_title {
+  text-align: center;
+  font-size: 80px;
+  color: #FFF;
+
+  font-family: Arial, sans-serif;
+}
+
+#section-1 {
+  background-image: url(https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/1_images/img-1920-800-4.jpg);
+  background-size: cover;
+}
+#section-2 {
+  background-image: url(https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/scroll-wheel-control/images/bg-3.jpg);
+  background-size: cover;
+}
+#section-3 {
+  background-image: url(https://raw.githubusercontent.com/SochavaAG/example-mycode/master/pens/scroll-wheel-control/images/bg-4.jpg);
+  background-size: cover;
+}
+.fp-controlArrow {
+  top: auto;
+  bottom: 7%;
+}
+.fp-controlArrow.fp-prev {
+  left: auto;
+  right: 200px;
+}
+.fp-controlArrow.fp-next {
+  right: 50px;
+}
+</style>
